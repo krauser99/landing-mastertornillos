937a52577356f1271549c611ac096ff55b1a5a49
krauser99 <spar34da@gmail.com>
2025-11-04 20:43:11 -0600

    Master tornillos Pagina

---
+++ js/productos.js
@@ -0,0 +1,149 @@
+const productos = 
+    [
+        {
+          "categoria": "Panaderia",
+          "nombre": "Pirujos y Pan Blanco",
+          "imagen": "img/productos/Pan Blanco.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Pan Hamburguesa",
+          "imagen": "img/productos/Pan Hamburguesa.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Pan Francés",
+          "imagen": "img/productos/Pan Frances.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Baguettes",
+          "imagen": "img/productos/Baguette.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Ciabattas",
+          "imagen": "img/productos/Ciabattas.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Pan Cubano",
+          "imagen": "img/productos/Pan Cubano.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Palitos de pan",
+          "imagen": "img/productos/Palitos de PAN.jpg"
+        },
+        {
+          "categoria": "Panaderia",
+          "nombre": "Estilos personalizados disponibles",
+          "imagen": "img/productos/Estilos personalizados pan.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Kekesitos",
+          "imagen": "img/productos/Cupcakes.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Brownies",
+          "imagen": "img/productos/Brownies.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Pasteles",
+          "imagen": "img/productos/Pasteles.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Croissants",
+          "imagen": "img/productos/Croissants.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Turron",
+          "imagen": "img/productos/Turrón.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Almibares",
+          "imagen": "img/productos/Almibares.jpg"
+        },
+        {
+          "categoria": "Reposteria",
+          "nombre": "Reposteria personalizada",
+          "imagen": "img/productos/Reposteria personalizada.jpg"
+        },
+        {
+          "categoria": "Salsas y Condimentos",
+          "nombre": "Salsas rojas y verdes",
+          "imagen": "img/productos/Salsa roja y verde.jpg"
+        },
+        {
+          "categoria": "Salsas y Condimentos",
+          "nombre": "Salsas picantes",
+          "imagen": "img/productos/Salsa picante.jpg"
+        },
+        {
+          "categoria": "Salsas y Condimentos",
+          "nombre": "Condimentos de todo tipo a medida",
+          "imagen": "img/productos/Condimentos.jpg"
+        },
+        {
+          "categoria": "Salsas y Condimentos",
+          "nombre": "Marinados",
+          "imagen": "img/productos/Marinados.jpg"
+        },
+        {
+          "categoria": "Salsas y Condimentos",
+          "nombre": "Diseño de salsa gourmet personalizadas",
+          "imagen": "img/productos/Diseño salsa personalizada.jpg"
+        },
+        {
+          "categoria": "Fritos",
+          "nombre": "Empanadas",
+          "imagen": "img/productos/Empanadas.jpg"
+        },
+        {
+          "categoria": "Fritos",
+          "nombre": "Papas fritas",
+          "imagen": "img/productos/Papas fritas.jpg"
+        },
+        {
+          "categoria": "Fritos",
+          "nombre": "Carnes preparadas",
+          "imagen": "img/productos/Carnes preparadas.jpg"
+        },
+        {
+          "categoria": "Fritos",
+          "nombre": "Personalización de productos para cadena de restaurantes",
+          "imagen": "img/productos/Personalización para restaurantes.jpg"
+        },
+        {
+          "categoria": "Desayuno",
+          "nombre": "Frijoles parados",
+          "imagen": "img/productos/Frijoles parados.jpg"
+        },
+        {
+          "categoria": "Desayuno",
+          "nombre": "Frijoles volteados",
+          "imagen": "img/productos/Frijoles volteados.jpg"
+        },
+        {
+          "categoria": "Desayuno",
+          "nombre": "Pan de desayuno",
+          "imagen": "img/productos/Pan de desayuno.jpg"
+        },
+        {
+          "categoria": "Desayuno",
+          "nombre": "Mermeladas",
+          "imagen": "img/productos/Mermeladas.jpg"
+        },
+        {
+          "categoria": "Desayuno",
+          "nombre": "Personalización de platos a medida",
+          "imagen": "img/productos/Personalización de plato a la medida.jpg"
+        }      
+  ];
+  
